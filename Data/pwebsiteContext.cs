f1265919529ec6dd3e511272f530351af3405467
Zhennan Tu <zhennan.tu@gmail.com>
2021-06-21 23:02:57 +0800

    next page

--- Data/pwebsiteContext.cs
+++ Data/pwebsiteContext.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -14,6 +14,7 @@ namespace pwebsite.Data
         {
         }
 
+        //TODO: 加数量限制
         public DbSet<pwebsite.Models.Summary> Summary { get; set; }
     }
 }
--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -18,6 +18,10 @@ namespace pwebsite.Pages
 
         public IList<Summary> Summary { get; set; }
 
+        public int CurrentPage { get; set; }
+
+        public bool IsLastPage { get; set; }
+
         //卧槽，我把logger和context位置换过也能跑对
         //怀疑传进来的参数是在某个全局单例"池"，根据类型取
         public IndexModel(ILogger<IndexModel> logger, pwebsite.Data.pwebsiteContext context)
@@ -26,9 +30,33 @@ namespace pwebsite.Pages
             _context = context;
         }
 
-        public async Task OnGetAsync()
+        public async Task OnGetAsync(int? p)
         {
-            Summary = await _context.Summary.ToListAsync();
+            if (p.HasValue)
+            {
+                Summary = await _context.Summary
+                    .OrderByDescending(summary => summary.ReleaseDate)
+                    .Skip(Math.Min(p.Value-1, 0) * 10)
+                    .Take(10)
+                    .ToListAsync();
+                CurrentPage = p.Value;
+            }
+            else
+            {
+                Summary = await _context.Summary
+                    .OrderByDescending(summary => summary.ReleaseDate)
+                    .Take(10)
+                    .ToListAsync();
+                CurrentPage = 1;
+            }
+            //if (Summary.Count() < 10)
+            //{
+            //    IsLastPage = true;
+            //}
+            //else
+            //{
+            //    IsLastPage = false;
+            //}
         }
     }
 }
